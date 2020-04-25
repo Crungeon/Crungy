978e2b52802f2253b87629a936a180fc711bd40e
Mike Kletz <michael.kletz.27@gmail.com>
2020-04-24 23:02:50 -0500

    typo

--- commands/sharktank.go
+++ commands/sharktank.go
@@ -14,7 +14,7 @@ func SharkTank(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "shark tank" {
 		s.ChannelMessageSend(m.ChannelID, "Mike has learned alot of valuable business information on shark tank primarily the following.")
-		s.ChannelMessageSend(m.ChannelID, "1) Has a proper valuation based on current business not potential business or you look like an idiot.")
+		s.ChannelMessageSend(m.ChannelID, "1) Have a proper valuation based on current business not potential business or you look like an idiot.")
 		s.ChannelMessageSend(m.ChannelID, "2) Building off #1 don't assume your shitty start up is going to get 20% market share of anything in a year.")
 		s.ChannelMessageSend(m.ChannelID, "3) Have a plan for what you're doing with the investor's money and be able to articulate what that plan is.")
 		s.ChannelMessageSend(m.ChannelID, "4) Patents are important. If you don't have one don't bother showing up. Also a registered design is not a patent you dumb dumb.")
