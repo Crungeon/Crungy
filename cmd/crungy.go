c0584b5397925ecaea6048000aaac1d32624066e
Mike Kletz <michael.kletz.27@gmail.com>
2020-04-24 23:46:24 -0500

    luketime

--- cmd/crungy.go
+++ cmd/crungy.go
@@ -116,7 +116,8 @@ func crungy(cmd *cobra.Command, args []string) {
 	dg.AddHandler(commands.PingPong)
 	dg.AddHandler(commands.Roasted)
 	dg.AddHandler(commands.SharkTank)
-	dg.AddHandler(commands.Love)
+	dg.AddHandler(commands.love)
+	dg.AddHandler(commands.luketime)
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
